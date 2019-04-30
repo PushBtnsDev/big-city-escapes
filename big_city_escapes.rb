b8d58b6c4ac3fd0598adca5cc446f5601475bb8b
PushBtnsDev <kyledchenoweth@gmail.com>
2019-04-30 17:56:08 +1000

    Removed brackets where not needed in main ruby file big_city_escapes

--- big_city_escapes.rb
+++ big_city_escapes.rb
@@ -5,39 +5,36 @@ require_relative 'locations_classes/melbourne'
 require_relative 'locations_classes/london'
 require_relative 'locations_classes/main_classes'
 
-
-def game_introduction()
+def game_introduction
     # returns a player object
     # introduces the game and scenario
 
     # INTRO GEM SCREEN
-    puts "\n" * 20
-    File.open("text_files/big_city_skyline.txt", "r") do |f|
+    puts "\n" * 50
+    File.open "text_files/big_city_skyline.txt", "r" do |f|
         f.each_line do |line|
             puts line
         end
     end
     title = Artii::Base.new :font => 'slant'
-    puts title.asciify('Big City Escapes!')
+    puts title.asciify 'Big City Escapes!'
 
     continue = 0
     while continue != 1
         print "Press 1 to begin: "
-        continue = gets.strip.to_i()
-        puts "\n" * 20
+        continue = gets.strip.to_i
+        puts "\n" * 50
     end
 
-    puts "\n" * 20
-    puts "Welcome to Big City Escapes! You are about to embark on a trip to 3 BIG cities! In each city you may get yourself stuck in some situations that you need to escape from.  Follow the prompts to get the best clues on how to escape from each dilemma you face, be careful along the way you may run into a few roadblocks.".yellow + " 
-
-    You will be playing for the ultimate prize of $1 million dollars tax free cash plus a donation of".yellow + " about 3 fiddy ".green + "to a charity of your choice. ".yellow
+    puts "\n" * 50
+    puts "Welcome to Big City Escapes! You are about to embark on a trip to 3 BIG cities! In each city you may get yourself stuck in some situations that you need to escape from.  Follow the prompts to get the best clues on how to escape from each dilemma you face, be careful along the way you may run into a few roadblocks.".yellow + "You will be playing for the ultimate prize of $1 million dollars tax free cash plus a donation of".yellow + " about 3 fiddy ".green + "to a charity of your choice. ".yellow
 
 
     continue = 0
     while continue != 1
         print "Press 1 to continue: "
-        continue = gets.strip.to_i()
-        puts "\n" * 20
+        continue = gets.strip.to_i
+        puts "\n" * 50
     end
 
     puts "Each player will start with a health of ".yellow + "100 Hearts.".red + " 
@@ -49,21 +46,21 @@ def game_introduction()
     while name.length < 0 || name.length > 6
         print "Please enter your name: "
         name = gets.strip
-        puts "\n" * 20
+        puts "\n" * 50
     end
 
-    return Player.new(name)
+    return Player.new name
 end
 
-def finish_line(player)
+def finish_line player
     
-    puts "Phwoah! What an excellent adventure! Well done, you've made it to the finish line in one piece. Let's see if your final score was enough to clinch that ultimate prize of $1 million dollars! (and about a 3 fiddy donation to a charity of your choice). You will need to be at the top scoring range to bring home the dough."
+    puts "Phwoah! What an excellent adventure! Well done, you've made it to the finish line in one piece. Let's see if your final score was enough to clinch that ultimate prize of ".yellow + "$1 million dollars!".green + " (and about a ".yellow + "3 fiddy donation".light_green + " to a charity of your choice). You will need to be at the top scoring range to bring home the dough.".yellow
 
     continue = 0
     while continue != 1
         print "Press 1 to view the highscore: "
-        continue = gets.strip.to_i()
-        puts "\n" * 20
+        continue = gets.strip.to_i
+        puts "\n" * 50
     end
 
     # scoreboard
@@ -75,7 +72,7 @@ def finish_line(player)
     count = 0
     while count < set_scoreboard.length && !inserted
         if player.score > set_scoreboard[count][1]
-            set_scoreboard.insert(count, player_score_block) 
+            set_scoreboard.insert count, player_score_block
             inserted = true
         end
         count += 1
@@ -94,47 +91,47 @@ def finish_line(player)
 end
 
 def finish_line_test
-    barry = Player.new("Barry")
-    barry.update_score(500)
+    barry = Player.new "Barry"
+    barry.update_score 500
 
     # Expect Barry to sit on the scoreboard between Jov and Jordan
-    finish_line(barry)
+    finish_line barry
 end
 
-def player_is_in_inventory_test()
+def player_is_in_inventory_test
 
-    player = Player.new("Player")
-    player.add_item("Dog")
+    player = Player.new "Player"
+    player.add_item "Dog"
 
    
-    if player.is_in_inventory?("Dog")
+    if player.is_in_inventory? "Dog"
         puts "player_is_in_inventory_test ran as expected"
     else
         puts "player_is_in_inventory_test did not run as expected"
     end
 
-    if player.is_in_inventory?("Cat")
+    if player.is_in_inventory? "Cat"
         puts "player_is_in_inventory_test did not run as expected"
     else
         puts "player_is_in_inventory_test ran as expected"
     end
 end
 
-def player_change_health_test()
+def player_change_health_test
     
     # Initial health = 100
-    player = Player.new("Player")
+    player = Player.new "Player"
 
-    player.change_health(-25)
+    player.change_health -25
     if player.health == 75
         puts "player_change_health_test ran as expected"
     else
         puts "player_change_health_test did not run as expected"
     end
 
-    player.change_health(-25)
-    player.change_health(-25)
-    if player.change_health(-25) == "Game Over, your journey has come to an end!"
+    player.change_health -25
+    player.change_health -25
+    if (player.change_health -25) == "Game Over, your journey has come to an end!"
         puts "player_change_health_test ran as expected"
     else
         puts "player_change_health_test did not run as expected"
@@ -142,22 +139,22 @@ def player_change_health_test()
 
 end
 
-def main_game()
+def main_game
     # Main function
 
     # introduction
-    player = game_introduction()
+    player = game_introduction
 
     dead = false
     # New York
     NewYork.new_york_greeting
-    continue = NewYork.boat_escape(player)
+    continue = NewYork.boat_escape player
     if !continue
         dead = true
     end
         
     if !dead
-        NewYork.roadblock1(player)
+        NewYork.roadblock1 player
     end
 
 
@@ -165,14 +162,14 @@ def main_game()
     
     if !dead
         Melbourne.melbourne_greeting
-        continue = Melbourne.aus_open_escape(player)
+        continue = Melbourne.aus_open_escape player
     end
     if !continue
         dead = true
     end
 
     if !dead
-        Melbourne.roadblock2(player)
+        Melbourne.roadblock2 player
     end
 
 
@@ -180,36 +177,36 @@ def main_game()
     
     if !dead
         London.london_greeting
-        continue = London.london_dungeon(player)
+        continue = London.london_dungeon player
     end
     if !continue
         dead = true
     end
 
     if !dead
-        London.roadblock3(player)
+        London.roadblock3 player
     end
 
     # Outro
     if !dead
-        finish_line(player)
+        finish_line player
         end_message = Artii::Base.new :font => 'slant'
-        puts end_message.asciify('You made it!')
+        puts end_message.asciify 'You made it!'
     end
 
     if dead
-        File.open("text_files/big_city_skull.txt", "r") do |f|
+        File.open "text_files/big_city_skull.txt", "r" do |f|
             f.each_line do |line|
                 puts line
             end
         end
         end_message = Artii::Base.new :font => 'slant'
-        puts end_message.asciify('Game Over')
+        puts end_message.asciify 'Game Over'
     end
 end
 
 def main
-    puts "\n" * 20
+    puts "\n" * 50
 
     playing = true
     while playing
@@ -217,4 +214,4 @@ def main
     end
 end
 
-main
+player_change_health_test
